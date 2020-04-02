0105846684fa1d17ff430c71f8de5b857cfc0c72
张鲁燕 <zhangluyan@jsh.com>
2020-04-02 23:11:05 +0800

    修改cat的样式

--- application/views/cat/cat_message.php
+++ application/views/cat/cat_message.php
@@ -3,8 +3,8 @@
 		<?php foreach ($items->result() as $item):?>
 			<div class="item_cat">
 				<img src="/uploads/<?php echo $item->url_path.'/'.$item->big_img;?>"/>
-				<p><?php echo $item->author;?></p>
-				<p><?php echo $item->title;?></p>
+				<p class="title-area"><?php echo $item->title;?></p>
+				<p class="author-area"><?php echo $item->author;?></p>
 			</div>
 		<?php endforeach;?>
 	</div>
