190131cf03769fb60b0f7d443d8865131574d1ce
张鲁燕 <zhangluyan@jsh.com>
2021-01-04 19:38:09 +0800

    update footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -2,7 +2,7 @@
 <script src="/static/js/main.js"></script>
 <div id="footer">
 	本站所有资源均来自互联网，如有侵权，请联系站长删除！<br />
-	网站备案/许可证号：<a herf="beian.miit.gov.cn">ICP备18001334号</a>
+	网站备案/许可证号：<a href="https://beian.miit.gov.cn/">ICP备18001334号</a>
 </div>
 </body>
 </html>
